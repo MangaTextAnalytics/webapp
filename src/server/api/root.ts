18960d1de37d8d1eb7a1e0c9cb4b397ed7f96e02
colmpat <cplang@dons.usfca.edu>
2023-10-17 16:17:55 -0700

    feat: add Libary to schema and clean up schema

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,5 @@
-import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
-import { mangaRouter } from "./routers/mangas";
+import { mangaRouter } from "~/server/api/routers/mangas";
 
 /**
  * This is the primary router for your server.
@@ -8,7 +7,6 @@ import { mangaRouter } from "./routers/mangas";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   mangas: mangaRouter,
 });
 
